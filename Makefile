a83aeaf0c3537f90d088b8cf0bb39710f2a7c6cb
Uwem Akan <uwemakan12@gmail.com>
2022-05-02 18:10:13 +0100

    add db docs

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+DB_URL=postgresql://postgres:password@localhost:5432/simple_bank?sslmode=disable
+
 postgres:
 	docker run --name pg1 --network bank-network -e POSTGRES_PASSWORD=password -d -p 5432:5432 postgres
 
@@ -8,16 +10,22 @@ dropdb:
 	docker exec -it pg1 dropdb --username=postgres simple_bank
 
 migrateup:
-	migrate -path db/migration/ -database "postgresql://postgres:password@localhost:5432/simple_bank?sslmode=disable" -verbose up
+	migrate -path db/migration/ -database "$(DB_URL)" -verbose up
 
 migrateup1:
-	migrate -path db/migration/ -database "postgresql://postgres:password@localhost:5432/simple_bank?sslmode=disable" -verbose up 1
+	migrate -path db/migration/ -database "$(DB_URL)" -verbose up 1
 
 migratedown:
-	migrate -path db/migration/ -database "postgresql://postgres:password@localhost:5432/simple_bank?sslmode=disable" -verbose down
+	migrate -path db/migration/ -database "$(DB_URL)" -verbose down
 
 migratedown1:
-	migrate -path db/migration/ -database "postgresql://postgres:password@localhost:5432/simple_bank?sslmode=disable" -verbose down 1
+	migrate -path db/migration/ -database "$(DB_URL)" -verbose down 1
+
+dbDocs:
+	dbdocs build doc/db.dbml
+
+dbSchema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
 
 sqlc:
 	sqlc generate
@@ -31,4 +39,4 @@ server:
 mock:
 	mockgen -package mockdb --build_flags=--mod=mod -destination db/mock/store.go github.com/uwemakan/simplebank/db/sqlc Store
 
-.PHONY: postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 sqlc test server mock
+.PHONY: postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 dbDocs dbSchema sqlc test server mock
---
+++ doc/schema.sql
@@ -0,0 +1,67 @@
+-- SQL dump generated using DBML (dbml-lang.org)
+-- Database: PostgreSQL
+-- Generated at: 2022-05-02T17:04:03.285Z
+
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" float NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" float NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "sender" varchar NOT NULL,
+  "recipient" varchar NOT NULL,
+  "amount" float NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("sender", "from_account_id");
+
+CREATE INDEX ON "transfers" ("sender", "to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be positive or negative';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("sender") REFERENCES "users" ("username");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("recipient") REFERENCES "users" ("username");
